ddfd228229feb717e08e5d4bb6338b82f7166906
trangtanphuoc <30283697+trangtanphuoc@users.noreply.github.com>
2017-08-13 09:12:59 -0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,51 @@
+package com.tanphuoc.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView descText;
+    ImageButton show, hide;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addControl();
+        addEvent();
+    }
+
+    private void addEvent() {
+        show.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("Show button");
+                show.setVisibility(View.INVISIBLE);
+                hide.setVisibility(View.VISIBLE);
+                descText.setMaxLines(Integer.MAX_VALUE);
+
+            }
+        });
+        hide = (ImageButton) findViewById(R.id.hide);
+        hide.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hide button");
+                hide.setVisibility(View.INVISIBLE);
+                show.setVisibility(View.VISIBLE);
+                descText.setMaxLines(1);
+
+            }
+        });
+    }
+
+    private void addControl() {
+        descText = (TextView) findViewById(R.id.description_text);
+        show = (ImageButton) findViewById(R.id.show);
+    }
+}
